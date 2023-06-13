7d2c025d8a6cbf5f75a943b20863a53fc3cb5265
Morgan Jones <morgan@doesstuff.dev>
2023-06-13 11:25:36 +0100

    feat: Many things
    Added SUGGESTED_TYPES
    
    Fixed args.len() >= 1
    
    Created format_message
    
    Added basic user input method
    
    Added unused commit function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use clap::Parser;
 use std::io;
 use std::io::Write;
 
+const SUGGESTED_TYPES: [&str; 10] = [
+    "fix", "feat", "build", "chore", "ci", "docs", "style", "refactor", "perf", "test",
+];
+
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short = 'm', long = "message")]
@@ -17,7 +21,7 @@ struct Args {
 fn main() {
     let args = std::env::args();
 
-    if args.len() >= 1 {
+    if args.len() > 1 {
         handle_with_clap();
     } else {
         handle_with_user_input();
@@ -32,11 +36,19 @@ fn handle_with_clap() {
         None => "".to_owned(),
     };
 
-    let message = format!("{}{}: {}", args.message_type, scope, args.message);
+    let message = format_message(args.message_type, scope, args.message);
     println!("{}", message);
 }
 
-fn handle_with_user_input() {}
+fn handle_with_user_input() {
+    // TODO: Add "popup" for SUGGESTED_TYPES
+    let message_type = get_user_input("What type of commit is this?", true);
+    let message = get_user_input("What is the commit message?", true);
+    let scope = get_user_input("What is the scope of this commit? (Optional)", false);
+
+    let message = format_message(message_type, scope, message);
+    println!("{}", message);
+}
 
 fn get_user_input(prompt: &str, required: bool) -> String {
     let mut input = String::new();
@@ -50,5 +62,22 @@ fn get_user_input(prompt: &str, required: bool) -> String {
         return get_user_input(prompt, required);
     }
 
-    return input;
+    return input.trim().to_owned();
+}
+
+fn format_message(message_type: String, scope: String, message: String) -> String {
+    let scope = match scope.trim().is_empty() {
+        true => "".to_owned(),
+        false => format!("({})", scope).to_string(),
+    };
+
+    return format!("{}{}: {}", message_type, scope, message);
+}
+
+fn commit(args: Vec<String>) {
+    std::process::Command::new("git")
+        .arg("commit")
+        .arg(args.join(" "))
+        .spawn()
+        .expect("Failed to commit");
 }
