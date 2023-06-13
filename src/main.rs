f45e09d6a8dc6a4ea374f3523acb92755007cec8
Morgan Jones <morgan@doesstuff.dev>
2023-06-13 11:14:19 +0100

    feat: Add scope option

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
+use clap::Parser;
 use std::io;
 use std::io::Write;
 
-fn main() {
-    let x = get_user_input("Enter a number", true);
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short = 'm', long = "message")]
+    message: String,
 
-    println!("You said: {x}");
+    #[arg(short = 't', long = "type")]
+    message_type: String,
+
+    #[arg(short = 's', long = "scope")]
+    scope: Option<String>,
 }
+
+fn main() {
+    let args = std::env::args();
+
+    if args.len() >= 1 {
+        handle_with_clap();
+    } else {
+        handle_with_user_input();
+    }
+}
+
+fn handle_with_clap() {
+    let args = Args::parse();
+
+    let scope = match args.scope {
+        Some(scope) => format!("({})", scope).to_string(),
+        None => "".to_owned(),
+    };
+
+    let message = format!("{}{}: {}", args.message_type, scope, args.message);
+    println!("{}", message);
+}
+
+fn handle_with_user_input() {}
 
 fn get_user_input(prompt: &str, required: bool) -> String {
     let mut input = String::new();
