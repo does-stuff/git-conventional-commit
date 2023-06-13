0841e9f98a612a9197ca68f743f6fd893eeceb28
Morgan Jones <morgan@doesstuff.dev>
2023-06-13 11:05:15 +0100

    feat: Read user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let x = get_user_input("Enter a number", true);
+
+    println!("You said: {x}");
 }
 
+fn get_user_input(prompt: &str, required: bool) -> String {
+    let mut input = String::new();
+    print!("{prompt}: ");
+    io::stdout().flush().unwrap(); // Print before read_line
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    if required && input.trim().is_empty() {
+        println!("Please type an input!");
+        return get_user_input(prompt, required);
+    }
+
+    return input;
+}
